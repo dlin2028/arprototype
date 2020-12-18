259cf9b6d0978c12749020e434f7f5ff08391341
David Lin <dlin2028@gmail.com>
2020-12-18 15:49:11 -0800

    basic functionality working

---
+++ arprototype/Assets/Scripts/CrosshairSceneLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CrosshairSceneLoader : MonoBehaviour
+{
+    public void LoadCHSceneAdditiveAsync(string sceneName)
+    {
+        StartCoroutine(loadCHSceneAsync(sceneName));
+    }
+
+    private IEnumerator loadCHSceneAsync(string sceneName)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+
+        while(!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+        CrosshairSceneOrigin sceneOrigin = FindObjectOfType<CrosshairSceneOrigin>();
+        sceneOrigin.RotateScene();
+    }
+}
---
+++ arprototype/Assets/Scripts/CrosshairSceneOrigin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CrosshairSceneOrigin : MonoBehaviour
+{
+    public GameObject Crosshair;
+
+    public void RotateScene()
+    {
+        transform.LookAt(Crosshair.transform.position);
+        foreach (var go in gameObject.scene.GetRootGameObjects())
+        {
+            go.transform.parent = transform;
+        }
+
+        transform.rotation = Camera.main.transform.rotation;
+    }
+}
